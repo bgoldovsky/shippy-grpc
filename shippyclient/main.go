7d8273cb399f95c1bac18b89ad84dbcbd32dffa5
bgoldovsky <setsunsoul@outlook.com>
2020-02-12 11:08:04 +0300

    fixed

--- shippyclient/main.go
+++ shippyclient/main.go
@@ -27,6 +27,16 @@ func parseFile(file string) (*pb.Consignment, error) {
 	return consignment, err
 }
 
+func nicePrint(c *pb.Consignment) {
+	log.Println("===========CONSIGNMENT DATA===========")
+	log.Printf("ID: %s\n", c.GetId())
+	log.Printf("Description: %s\n", c.GetDescription())
+	log.Printf("Weight: %d\n", c.GetWeight())
+	log.Printf("Vessel: %s\n", c.GetVesselId())
+	log.Println("======================================")
+	log.Println()
+}
+
 func main() {
 
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
@@ -58,6 +68,6 @@ func main() {
 		log.Fatalf("Could not list consignments: %v", err)
 	}
 	for _, v := range getAll.Consignments {
-		log.Println(v)
+		nicePrint(v)
 	}
 }
